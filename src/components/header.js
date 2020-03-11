3604408432226a0f30535c7a4b22364bc190e85b
1 <y>
2020-03-11 17:18:16 +0100

    styled mobile's headers's email + copyToClipBoard

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Link from 'gatsby-link'
+import { CopyToClipboard } from 'react-copy-to-clipboard'
 
 const Header = ({ showMenuOrEmail, menuOrEmail }) => {
+  // const [copyValue, onCopyValue] = useState(false);
+  const onCopyValue = (text, result) => {
+    console.log(text, result)
+  }
   const email = 'youyou1youcef@gmail.com'
   const reversedEmail = email
     .split('')
@@ -95,6 +100,23 @@ const Header = ({ showMenuOrEmail, menuOrEmail }) => {
           <span style={{ direction: 'rtl', unicodeBidi: 'bidi-override' }}>
             {reversedEmail}
           </span>
+          <CopyToClipboard onCopy={onCopyValue} text={email}>
+            <button className="copyToClipboard">
+              <svg
+                className="octicon octicon-clippy"
+                viewBox="0 0 14 16"
+                version="1.1"
+                width="14"
+                height="16"
+                aria-hidden="true"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M2 13h4v1H2v-1zm5-6H2v1h5V7zm2 3V8l-3 3 3 3v-2h5v-2H9zM4.5 9H2v1h2.5V9zM2 12h2.5v-1H2v1zm9 1h1v2c-.02.28-.11.52-.3.7-.19.18-.42.28-.7.3H1c-.55 0-1-.45-1-1V4c0-.55.45-1 1-1h3c0-1.11.89-2 2-2 1.11 0 2 .89 2 2h3c.55 0 1 .45 1 1v5h-1V6H1v9h10v-2zM2 5h8c0-.55-.45-1-1-1H8c-.55 0-1-.45-1-1s-.45-1-1-1-1 .45-1 1-.45 1-1 1H3c-.55 0-1 .45-1 1z"
+                ></path>
+              </svg>
+            </button>
+          </CopyToClipboard>
         </div>
       )}
       <div className="sousMenu">
--- src/components/layout.js
+++ src/components/layout.js
@@ -3,25 +3,25 @@ import { Helmet } from 'react-helmet'
 import MobHeader from './MobHeader'
 import Header from './header'
 import Splash from './Splash'
-import { StaticQuery, graphql } from "gatsby"
+import { StaticQuery, graphql } from 'gatsby'
 import '../layouts/index.scss'
 import '../layouts/main.sass'
 import '../layouts/splash.sass'
 
 export default ({ children, fromLink }) => {
-  const [menuOrEmail, showMenuOrEmail] = useState("");
+  const [menuOrEmail, showMenuOrEmail] = useState('')
 
   return (
     <StaticQuery
       query={graphql`
-      query LayoutQuery {
-        site {
-          siteMetadata {
-            title
+        query LayoutQuery {
+          site {
+            siteMetadata {
+              title
+            }
           }
         }
-      }
-    `}
+      `}
       render={data => (
         <div className="root">
           <Helmet
@@ -33,8 +33,14 @@ export default ({ children, fromLink }) => {
           />
           {!fromLink && <Splash />}
           <MobHeader showMenuOrEmail={showMenuOrEmail} />
-          <div className="layout" style={!menuOrEmail.length ? {} : { position: 'fixed' }}>
-            <Header showMenuOrEmail={showMenuOrEmail} menuOrEmail={menuOrEmail} />
+          <div
+            className="layout"
+            style={!menuOrEmail.length ? {} : { position: 'fixed' }}
+          >
+            <Header
+              showMenuOrEmail={showMenuOrEmail}
+              menuOrEmail={menuOrEmail}
+            />
             {children}
           </div>
         </div>
